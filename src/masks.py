5d04d1699a5e6b485440e7648440329e67d25f3e
Эдгар Торозян <edgartorozyan@192.168.1.2>
2024-06-16 16:48:13 +0300

    add pythonProject(homework)

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(number: str) -> str:
+    """Функция, которая возвращает маскированный номер карты"""
+    new_num = str(number)
+    if len(new_num) == 16:
+        return f"{new_num[:4]} {new_num[4:6]}** **** {new_num[12:]}"
+
+
+def get_mask_account(number: str) -> str:
+    """Функция, которая возвращает маскированный номер счета"""
+    new_num = str(number)
+    if len(new_num) == 20:
+        return f"**{new_num[-4:]}"
---
+++ src/widget.py
@@ -0,0 +1,23 @@
+import masks
+
+
+def mask_account_card(string: str) -> str:
+    """Функция для маскировки счетев и карт"""
+    if "Счет" in string:
+        account = string[-20:]
+        return string[:20] + masks.get_mask_account(account)
+    else:
+        number_card = "".join(string[-16:].split())
+        return string[:-16] + masks.get_mask_card_number(number_card)
+
+
+# print(mask_account_card('Maestro:1596837868705122422'))
+
+def get_data(info_data: str) -> str:
+    """Фунция преоброзования даты и времени"""
+    data_day = info_data.split("T")[0]
+
+    return f"{data_day.split('-')[-1]}.{data_day.split('-')[-2]}.{data_day.split('-')[-3]}"
+
+
+# print(get_data('2018-07-11T02:26:18.671407'))
