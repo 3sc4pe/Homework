c8fb2ed00d2e5fd39a7f6ec21a198f237503366a
edgartorozyan <torozyan_edgar@mail.ru>
2024-06-24 10:02:19 +0300

    fixes for dev

--- src/processing.py
+++ src/processing.py
@@ -1,27 +1,18 @@
-def filter_by_state(dictionary_list: list, state: str = "EXECUTED") -> list:
+def filter_by_state(info_users: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Принимает словари и возвращает новый список из переданных значений"""
-    new_list = []
-    for i in dictionary_list:
-        if i.get("state") == state:
-            new_list.append(i)
-        elif i.get("state") != state:
-            new_list.append(i)
-    return new_list
+    sort_info_users = []
+    for info in info_users:
+        if info["state"] == state:
+            sort_info_users.append(info)
+
+    return sort_info_users
 
 
-def sort_by_date(date_list: list[dict], reverse_list: bool) -> list:
+def sort_by_date(info_dicts: list[dict], sorting_parameter: bool = True) -> list[dict]:
     """Функция для сортировки полученных значений"""
-    sorted_list = sorted(date_list, key=lambda d: d["date"])
-    return sorted_list
+    if sorting_parameter is not True:
+        return sorted(info_dicts, key=lambda date: date["date"])
+    else:
+        return sorted(info_dicts, key=lambda date: date["date"], reverse=sorting_parameter)
 
 
-print(
-    filter_by_state(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
