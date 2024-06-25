bd07d66f6793b530f4cc70abd36a7187b4500d56
edgartorozyan <torozyan_edgar@mail.ru>
2024-06-25 13:02:35 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,8 +4,8 @@ from src.widget import mask_account_card
 
 
 @pytest.mark.parametrize(
-    "card_account, masks_card_account",
+    "account, number_card",
     [("Visa 1234567890123456", "Visa 1234 56** **** 3456"), ("Счет 12345678901234567890", "Счет **7890")],
 )
-def test_mask_account_card(card_account, masks_card_account):
-    assert mask_account_card(card_account) == masks_card_account
+def test_mask_account_card(account, number_card):
+    assert mask_account_card(account) == number_card
